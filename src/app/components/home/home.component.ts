b7ec0c7431204a6958dd07b80931a4f3801ba3c6
richarddlb <richardlopezbo.ar@gmail.com>
2025-06-19 19:07:14 -0300

    se

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,11 +7,10 @@ import { ProyectosComponent } from "../proyectos/proyectos.component";
 import { ContactoComponent } from "../contacto/contacto.component";
 import { FooterComponent } from "../footer/footer.component";
 import { FloatingButtonComponent } from "../floating-button/floating-button.component";
-import { FormLoginComponent } from "../form-login/form-login.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavComponent, BannerComponent, EstudiosComponent, ExperienciaComponent, ProyectosComponent, ContactoComponent, FooterComponent, FloatingButtonComponent, FormLoginComponent],
+  imports: [NavComponent, BannerComponent, EstudiosComponent, ExperienciaComponent, ProyectosComponent, ContactoComponent, FooterComponent, FloatingButtonComponent,],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
