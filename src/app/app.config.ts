6f4e408cd61ef5d85709c7ec36299690bad96df0
richarddlb <richardlopezbo.ar@gmail.com>
2025-06-19 19:25:13 -0300

    se modifica el archivo app.config.ts para intentar solucionar el problema de la pantalla en blanco

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withDebugTracing } from '@angular/router'; // Importación correcta
+import { provideRouter, withDebugTracing, withHashLocation } from '@angular/router'; // Importación correcta
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    // Configuración básica del router sin withHashLocation
-    provideRouter(routes)
+    // Configuración básica del router usando withHashLocation
+    provideRouter(routes, withHashLocation())
     
     // Si realmente necesitas HashLocationStrategy, usa esta alternativa:
-    // provideRouter(routes, { useHash: true })
+    // provideRouter(routes, withHashLocation())
   ]
 };
