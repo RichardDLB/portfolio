d3effb89e1a7185fb40f347ffba8dad4790b6779
richarddlb <richardlopezbo.ar@gmail.com>
2026-01-22 14:28:04 -0300

    se organiza la descar del cv en españo o ingles , se cambia el texto de las redes por iconos sgv y se modifica el tamaño de la letra en todos los componentes

--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -17,13 +17,25 @@ export class ExperienciaComponent {
             title: 'Landing Page Tintico',
             institution: 'Cafetería de especialidad Tintico',
             page: 'https://tinticocafeteria.github.io/Tintico/',
-            description: 'Desarrollado con el objetivo de crear una presencia digital atractiva y funcional. Utilizamos tecnologías modernas como HTML5 para la estructura del contenido, CSS3 y Bootstrap para un diseño responsive y adaptable a cualquier dispositivo, y JavaScript para agregar interactividad y dinamismo. Además, se emplearon plantillas de BootstrapMade para optimizar el proceso de diseño y garantizar un resultado profesional. El código fuente se gestionó mediante Git, lo que permitió un control de versiones eficiente y colaborativo. Finalmente, la página se despliega utilizando el servicio de hosting de GitHub'
+            description: 'Desarrollado con el objetivo de crear una presencia digital atractiva y funcional. Utilizamos tecnologías modernas como HTML5 para la estructura del contenido, CSS3 y Bootstrap para un diseño responsive y adaptable a cualquier dispositivo, JavaScript para agregar interactividad y dinamismo. Además, se emplearon plantillas de BootstrapMade para optimizar el proceso de diseño y garantizar un resultado profesional. El código fuente se gestionó mediante Git, lo que permitió un control de versiones eficiente y colaborativo. Finalmente, la página se despliega utilizando el servicio de hosting de GitHub.'
         },
         {
             title: 'Landing Page El Ingeniero C&L',
             institution: 'Ferretería y materiales para la construcción El Ingeniero C&L',
             page: 'https://elingenierocyl.github.io/ferreteria/index.html',
-            description: 'Se utilizaron tecnologías modernas y eficientes para garantizar un diseño atractivo, funcional y responsive. Utilizamos HTML5 para la estructura del contenido, CSS3 y Bootstrap para el diseño y la adaptabilidad en diferentes dispositivos, y JavaScript para agregar interactividad y dinamismo. Además, se emplearon plantillas de BootstrapMade para optimizar el proceso de diseño y asegurar un resultado profesional. El código fuente se gestionó mediante Git. Finalmente, la página se despliega utilizando el servicio de hosting de GitHub, asegurando un acceso rápido y confiable para los usuarios'
+            description: 'Se utilizaron tecnologías modernas y eficientes para garantizar un diseño atractivo, funcional y responsive. Utilizamos HTML5 para la estructura del contenido, CSS3 y Bootstrap para el diseño y la adaptabilidad en diferentes dispositivos, y JavaScript para agregar interactividad y dinamismo. Además, se emplearon plantillas de BootstrapMade para optimizar el proceso de diseño y asegurar un resultado profesional. El código fuente se gestionó mediante Git. Finalmente, la página se despliega utilizando el servicio de hosting de GitHub, asegurando un acceso rápido y confiable para los usuarios.'
+        },
+        {
+            title: 'Landing Page M. Rubio Topografía',
+            institution: 'Empresa especializada en servicios topográficos M. Rubio Topografía',
+            page: 'https://mrtopografia.github.io/MRTopografia/',
+            description: 'Diseño y desarrollo de la página web para MR Topografía, una empresa especializada en servicios topográficos y geodésicos de alta precisión. El objetivo era crear una presencia digital que transmitiera profesionalismo, confianza y expertise técnico. Tecnologías modernas como HTML5 para la estructura del contenido, CSS3 y Bootstrap para un diseño responsive y adaptable a cualquier dispositivo.'
+        },
+        {
+            title: 'Landing Page Carlitos Performance',
+            institution: 'Empresa especializada en servicios electromicánicos',
+            page: 'https://tallercarlitosperformance.github.io/carlitosperformance/',
+            description: 'Desarrollo del sitio web oficial para CarlitosPerformance, un taller mecánico con más de dos décadas de trayectoria. Este proyecto me permitió aplicar mis habilidades en desarrollo front-end para crear una presencia digital profesional y funcional que refleja los valores de honestidad y calidad del negocio.'
         }
     ];
 
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
-export class NavComponent {
 
+export class NavComponent {
+  showCvModal = false;
+
+  openCvModal() {
+    this.showCvModal = true;
+  }
+
+  closeCvModal() {
+    this.showCvModal = false;
+  }
+
+  downloadCv(lang: 'es' | 'en') {
+    let url = '';
+
+    if (lang === 'es') {
+      url = './assets/docs/richardCVEsp.pdf';
+    } else {
+      url = './assets/docs/richardCVEng.pdf';
+    }
+
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = lang === 'es' ? 'Richard_CV_ES.pdf' : 'Richard_CV_EN.pdf';
+    link.click();
+
+    this.closeCvModal();
+  }
 }
+
